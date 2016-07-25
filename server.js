b2534a793e9fd828eb4292817a0d61d48a17f46e
jcsfbass <jcsfbass@gmail.com>
2016-07-24 23:14:21 -0300

    Codigo feito na aula de ontem

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const mongodb = require('mongodb');
 const mongoClient = mongodb.MongoClient;
+const ObjectId = mongodb.ObjectId;
+
+const MONGODB_URI = 'mongodb://localhost:27017/jedi';
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -21,32 +24,73 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
 	if (req.session.user) {
-		mongoClient.connect('mongodb://localhost:27017/jedi', (err, db) => {
+		mongoClient.connect(MONGODB_URI, (err, db) => {
 			const usuarios = db.collection('usuarios');
-			usuarios.find({'_id': req.session.user.id}).toArray((err, docs) => {
-				console.log(docs);
+			usuarios.find({'_id': new ObjectId(req.session.user.id)}).toArray((err, docs) => {
+				db.close();
+				res.render('home', {nome: docs[0].nome});
 			});
-			/*usuarios.find({'_id': req.session.user.id}, (err, result) => {
-				console.log(result);
-			});*/
 		});
-		res.render('home', {id: req.session.user.id});
 	} else {
 		res.render('cadastro-login');
 	}
 });
 
-app.post('/cadastrar', (req, res) => {
-	mongoClient.connect('mongodb://localhost:27017/jedi', (err, db) => {
+app.post('/login', (req, res) => {
+	const email = req.body.email;
+	const senha = req.body.senha;
+
+	mongoClient.connect(MONGODB_URI, (err, db) => {
 		const usuarios = db.collection('usuarios');
-		usuarios.insertOne(req.body, (err, result) => {
+		usuarios.find({
+			'email': email,
+			'senha': senha
+		}).toArray((err, docs) => {
+			if (docs.length === 0) {
+				res.render('home', {nome: 'Usuário não cadastrado'});	
+			} else {
+				req.session.user = {id: docs[0]._id};
+				res.redirect('/');				
+			}
+
+			db.close();
+		});
+	});
+});
+
+app.get('/logout', (req, res) => {
+	delete req.session.user;
+	res.redirect('/');
+});
+
+app.post('/cadastrar', (req, res) => {
+	mongoClient.connect(MONGODB_URI, (err, db) => {
+		const usuarios = db.collection('usuarios');
+
+		const usuario = req.body;
+		usuario.posts = [];
+
+		usuarios.insertOne(usuario, (err, result) => {
 			db.close();
 
-			req.session.user = {
-				id: result.ops[0]._id
-			};
-
+			req.session.user = {id: result.ops[0]._id};
 			res.redirect('/');
+		});
+	});
+});
+
+app.post('/postagem', (req, res) => {
+	mongoClient.connect(MONGODB_URI, (err, db) => {
+		const usuarios = db.collection('usuarios');
+		usuarios.find({'_id': new ObjectId(req.session.user.id)}).toArray((err, docs) => {
+			const posts = docs[0].posts;
+			posts.push({
+				texto: req.body.texto
+			});
+
+			
+
+			db.close();
 		});
 	});
 });
